22b5544c2427f3c592c55ab1d083ed8e1f88652e
Juliette Gerbaux <juliette.gerbaux@rte-france.com>
2023-11-22 08:43:36 +0100

    Model with different zones

--- functions_watervalues.py
+++ functions_watervalues.py
@@ -1,62 +1,82 @@
 import numpy as np
 import xpress as xp
 xp.controls.outputlog = 0
-from time import time
-import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from param_watervalues import *
+import matplotlib.pyplot as plt
 
-def optModel_thermic():
+def optModel_thermic(study: Study):
     model = xp.problem()
     model.controls.xslp_log = -1
 
-    # LOAD
-     
-    w = [xp.var(f"load_{i}",lb = float('-inf'), ub =  float('inf')) for i in range(H)]
+    total_cost = [0]*H
 
-    model.addVariable (w) # Uncertain
+    imports = {}
+    exports = {}
+    for area in study.list_areas:
+        imports[area.name] = [0]*H
+        exports[area.name] = [0]*H
+    for a1 in study.list_areas:
+        for a2 in study.list_areas:
+            if a2.name in study.links[a1.name]:
+                exchange = [xp.var(f"exchange_{a1.name}_{a2.name}_{h}",lb = 0, ub =  study.links[a1.name][a2.name][0]) for h in range(H)] 
+                model.addVariable(exchange)
+                for h in range(H):
+                    imports[a2.name][h] += exchange[h]
+                    exports[a1.name][h] += exchange[h]
+                    total_cost[h] += exchange[h]*study.links[a1.name][a2.name][1]
+
+
 
     # CONTROLS
-    # Thermal 1
-    t1 = [xp.var(f"t1_{i}",lb = PminTh[0], ub =  PmaxTh[0]) for i in range(H)]
+    # Thermal
+    for area in study.list_areas:
 
-    model.addVariable (t1)      
-    # Thermal 2
-    t2 = [xp.var(f"t2_{i}",lb = PminTh[1], ub =  PmaxTh[1]) for i in range(H)]
+        total_prod = [0]*H
 
-    model.addVariable (t2)      # Second thermal unit       
-    # Thermal 3
-    t3 = [xp.var(f"t3_{i}",lb = PminTh[2], ub =  PmaxTh[2]) for i in range(H)]
+        # LOAD
+        w = [xp.var(f"load_{area.name}_{i}",lb = float('-inf'), ub =  float('inf')) for i in range(H)]
+        model.addVariable (w) # Uncertain
 
-    model.addVariable (t3)        # Third thermal unit       
-    # Not served energy
-    t4p = [xp.var(f"ens_{i}") for i in range(H)]
+        for thermal_unit in area.list_thermal_units :
+            t = [xp.var(f"t_{area.name}_{thermal_unit.name}_{i}",lb = thermal_unit.P_min, ub =  thermal_unit.P_max) for i in range(H)]
+            model.addVariable (t) 
+            for h in range(H):
+                total_prod[h] += t[h]
+                total_cost[h] += t[h]*thermal_unit.marginal_cost
+       
+        # Not served energy
+        ens = [xp.var(f"ens_{area.name}_{i}") for i in range(H)]
+        model.addVariable (ens)                    # Energy not served 
+        for h in range(H):
+            total_cost[h] += ens[h]*study.cost_ens
 
-    model.addVariable (t4p)                    # Energy not served 
-    t4n = [xp.var(f"spill_{i}") for i in range(H)]
+        spill = [xp.var(f"spill_{area.name}_{i}") for i in range(H)]
+        model.addVariable (spill) # Energy excess
 
-    model.addVariable (t4n) # Energy excess                               
+        # Interco
+                            
 
-    # Energy balnce
-    surplus_thermic = [xp.var(f"surplus_thermic_{h}",lb = float('-inf'), ub =  float('inf')) for h in range(H)]
-    model.addVariable(surplus_thermic)
-    for h in range(H):
-        model.addConstraint(surplus_thermic[h] == t1[h] + t2[h] + t3[h] + t4p[h] - w[h] - t4n[h])
+        # Energy balnce
+        surplus_thermic = [xp.var(f"surplus_thermic_{area.name}_{h}",lb = float('-inf'), ub =  float('inf')) for h in range(H)]
+        model.addVariable(surplus_thermic)
+        for h in range(H):
+            model.addConstraint(surplus_thermic[h] == total_prod[h] + ens[h] + imports[area.name][h] - w[h] - spill[h] - exports[area.name][h])
 
     cost_thermic = xp.var("cost_thermic",lb = float('-inf'), ub =  float('inf'))
     model.addVariable(cost_thermic)
-    model.addConstraint(cost_thermic==xp.Sum(np.transpose(np.tile(cTh,(168,1))) * np.array([t1, t2, t3, t4p])))
+    model.addConstraint(cost_thermic==xp.Sum(total_cost))
 
     return(model)
 
-def optModel_exact(s, V, debut):#x_s,inflow,w
-    model = optModel_thermic()
+def optModel_exact(study: Study, area: Area, reservoir: Reservoir, s, V, debut, X):#x_s,inflow,w
+    model = optModel_thermic(study)
 
     # STATE
-    x_s = xp.var("x_s",lb = XminB, ub = XmaxB)
+    x_s = xp.var("x_s",lb = 0, ub = reservoir.capacity)
     model.addVariable (x_s)          # State at the begining of the current week
 
-    x_s_1 = xp.var("x_s_1",lb = XminB, ub = XmaxB)
+    x_s_1 = xp.var("x_s_1",lb = 0, ub = reservoir.capacity)
     model.addVariable (x_s_1) # State at the begining of the following week
 
     q = [xp.var(f"q_{i}",lb = float('-inf'), ub =  float('inf')) for i in range(H+1)]
@@ -68,7 +88,7 @@ def optModel_exact(s, V, debut):#x_s,inflow,w
 
     # CONTROLS
     # Battery (>0 : pompage)
-    r = [xp.var(f"r_{i}",lb = PminB, ub =  PmaxB) for i in range(H)]
+    r = [xp.var(f"r_{i}",lb = reservoir.P_turb, ub =  reservoir.P_pump) for i in range(H)]
 
     model.addVariable (r)           # Charge and discharge control for the battery: r>0 means charge                          
 
@@ -81,9 +101,16 @@ def optModel_exact(s, V, debut):#x_s,inflow,w
 
     # Energy balnce
     var_thermic = model.getVariable()
-    surplus_thermic = [x for x in var_thermic if "surplus_thermic" in x.name]
+    surplus_thermic = [x for x in var_thermic if f"surplus_thermic_{area.name}_" in x.name]
     for h in range(H):
         model.addConstraint(surplus_thermic[h] == r[h])
+    
+    for a in study.list_areas:
+        if a.name != area.name:
+            surplus_thermic = [x for x in var_thermic if f"surplus_thermic_{a.name}_" in x.name]
+            for h in range(H):
+                model.addConstraint(surplus_thermic[h] == 0)
+
 
     # Battery dynamics
     model.addConstraint(q[0] == x_s)                           # State of the current week equal to the initial stock of the reservoir for the week
@@ -93,20 +120,20 @@ def optModel_exact(s, V, debut):#x_s,inflow,w
         model.addConstraint(q[h+1] == q[h] + r[h] + inflow)               # Stock evolution equation
 
     # Future cost: piecewise representation of the future cost
-    for i in range(xNsteps-1):
+    for i in range(len(X)-1):
         if (V[i+1, s+1]<float('inf'))&(V[i, s+1]<float('inf')):
             model.addConstraint(z >= (V[i+1, s+1] - V[i, s+1]) / (X[i+1] - X[i]) * (x_s_1 - X[i]) + V[i, s+1])
     
     if debut: 
         if s>=1:
-            model.addConstraint(y >=  -pen_low* (x_s - Xmin[s-1]))
-            model.addConstraint(y >=  pen_high* (x_s - Xmax[s-1]))
+            model.addConstraint(y >=  -study.pen_low* (x_s - reservoir.Xmin[s-1]))
+            model.addConstraint(y >=  study.pen_high* (x_s - reservoir.Xmax[s-1]))
         else:
-            model.addConstraint(y >=  -pen_low* (x_s - Xmin[S-1]))
-            model.addConstraint(y >=  pen_high* (x_s - Xmax[S-1]))
+            model.addConstraint(y >=  -study.pen_low* (x_s - reservoir.Xmin[S-1]))
+            model.addConstraint(y >=  study.pen_high* (x_s - reservoir.Xmax[S-1]))
     else:
-        model.addConstraint(y >=  -pen_low* (x_s_1 - Xmin[s]))
-        model.addConstraint(y >=  pen_high* (x_s_1 - Xmax[s]))
+        model.addConstraint(y >=  -study.pen_low* (x_s_1 - reservoir.Xmin[s]))
+        model.addConstraint(y >=  study.pen_high* (x_s_1 - reservoir.Xmax[s]))
 
     cost_thermic = [x for x in var_thermic if "cost_thermic" in x.name]
     model.setObjective(xp.Sum(cost_thermic) + z + y)
@@ -114,43 +141,49 @@ def optModel_exact(s, V, debut):#x_s,inflow,w
     return (model)
 
 
-def SDP_exact(debut):
-    V = np.zeros((xNsteps, S+1))
+def SDP_exact(study: Study, area, reservoir, debut, X):
+    V = np.zeros((len(X), S+1))
     if debut:
         pen = get_penalties(debut,S)
-        for i in range(xNsteps):
+        for i in range(len(X)):
             V[i,S] = pen(X[i])
 
     for s in range(51,-1,-1):
-        model = optModel_exact(s, V, debut)
-        for i in range(xNsteps): # state boucle x 
+        model = optModel_exact(study, area, reservoir, s, V, debut, X)
+        for i in range(len(X)): # state boucle x 
             Vx = 0
             model.chgbounds(["x_s","x_s"],['L','U'],[X[i],X[i]])
-            for k in range(NTrain): # chronicle boucle w 
-                model.chgbounds(["inflow","inflow"],['L','U'],[apport[s,k],apport[s,k]])
-                model.chgbounds([f"load_{j}" for j in range(H)]*2,['L']*H+['U']*H,list(LAW_s[s,:,k])+list(LAW_s[s,:,k]))
+            for k in range(study.nb_mc): # chronicle boucle w 
+                model.chgbounds(["inflow","inflow"],['L','U'],[reservoir.inflow[s,k],reservoir.inflow[s,k]])
+                for a in study.list_areas:
+                    model.chgbounds([f"load_{a.name}_{j}" for j in range(H)]*2,['L']*H+['U']*H,list(a.load[s,:,k])+list(a.load[s,:,k]))
                 model.solve()
                 Vx = Vx + model.getObjVal()
-            V[i, s] = Vx/NTrain
+            V[i, s] = Vx/study.nb_mc
     return (V)
 
-def OpModel_weeklycost():
-    model = optModel_thermic()
+def OpModel_weeklycost(study, area, reservoir):
+    model = optModel_thermic(study)
 
-    u = xp.var("u")
+    u = xp.var("u",lb = float('-inf'), ub =  float('inf'))
     model.addVariable (u)          # total turb
 
     # CONTROLS
     # Battery
-    r = [xp.var(f"r_{i}",lb = PminB, ub =  PmaxB) for i in range(H)]
+    r = [xp.var(f"r_{i}",lb = reservoir.P_turb, ub =  reservoir.P_pump) for i in range(H)]
 
     model.addVariable (r)           # Charge and discharge control for the battery: r>0 means charge
     
     # Energy balnce
     var_thermic = model.getVariable()
-    surplus_thermic = [x for x in var_thermic if "surplus_thermic" in x.name]
+    surplus_thermic = [x for x in var_thermic if f"surplus_thermic_{area.name}_" in x.name]
     for h in range(H):
         model.addConstraint(surplus_thermic[h] == r[h])
+    for a in study.list_areas:
+        if a.name != area.name:
+            surplus_thermic = [x for x in var_thermic if f"surplus_thermic_{a.name}_" in x.name]
+            for h in range(H):
+                model.addConstraint(surplus_thermic[h] == 0)
 
     # Battery dynamics
     model.addConstraint(xp.Sum(r) == -u)
@@ -161,39 +194,40 @@ def OpModel_weeklycost():
     return (model)
 
 
-def WeeklyCost(u):
-    model = OpModel_weeklycost()
-    L = np.zeros((S,NTrain))
+def WeeklyCost(study, area, reservoir, u):
+    model = OpModel_weeklycost(study, area, reservoir)
+    L = np.zeros((S,study.nb_mc))
     model.chgbounds(["u","u"],['L','U'],[u,u])
-    for k in range(NTrain):
+    for k in range(study.nb_mc):
         for s in range(S):
-            model.chgbounds([f"load_{j}" for j in range(H)]*2,['L']*H+['U']*H,list(LAW_s[s,:,k])+list(LAW_s[s,:,k]))
+            for a in study.list_areas:
+                model.chgbounds([f"load_{a.name}_{j}" for j in range(H)]*2,['L']*H+['U']*H,list(a.load[s,:,k])+list(a.load[s,:,k]))
             model.solve()
             L[s,k] = model.getObjVal()
     return(L)
 
-def SDP_precalculated_rewards(debut, Gu):    
-    V = np.zeros((xNsteps, S+1))
+def SDP_precalculated_rewards(study, reservoir, debut, Gu, X, U):    
+    V = np.zeros((len(X), S+1))
 
     if debut:
-        pen = get_penalties(debut,S)
-        for i in range(xNsteps):
+        pen = get_penalties(study, reservoir,debut,S)
+        for i in range(len(X)):
             V[i,S] = pen(X[i])
 
     for s in range(S-1,-1,-1):
         V_fut = interp1d(X, V[:, s+1])
-        pen = get_penalties(debut,s)
+        pen = get_penalties(study, reservoir,debut,s)
 
-        for k in range(NTrain):
+        for k in range(study.nb_mc):
             Gs = interp1d(U, Gu[:, s, k])
-            for i in range(xNsteps):
+            for i in range(len(X)):
                 Vu = float('-inf')
 
                 if debut :
                     penalty = pen(X[i])
                 
-                for i_fut in range(xNsteps):
-                    u = -X[i_fut] + X[i] + apport[s,k]*H
+                for i_fut in range(len(X)):
+                    u = -X[i_fut] + X[i] + reservoir.inflow[s,k]*H
                     if np.min(U) <= u <= np.min(U):
                         G = Gs(u)
                         if not(debut):
@@ -201,39 +235,96 @@ def SDP_precalculated_rewards(debut, Gu):
                         if (G + V[i_fut, s+1]+penalty) > Vu:
                             Vu = G + V[i_fut, s+1]+penalty
 
-                for u in range(Ncontrols):
-                    state_fut = X[i] - U[u] + apport[s,k]*H 
-                    if XminB <= state_fut <= XmaxB:
+                for u in range(len(U)):
+                    state_fut = X[i] - U[u] + reservoir.inflow[s,k]*H 
+                    if 0 <= state_fut <= reservoir.capacity:
                         if not(debut):
                             penalty = pen(state_fut)
                         if (Gu[u, s, k] + V_fut(state_fut)+penalty) > Vu:
                             Vu = (Gu[u, s, k] + V_fut(state_fut)+penalty)
 
-                Umin = X[i]+ apport[s,k]*H-Xmin[s]
+                Umin = X[i]+ reservoir.inflow[s,k]*H-reservoir.Xmin[s]
                 if np.min(U) <= Umin <= np.max(U):
-                    state_fut = X[i] - Umin + apport[s,k]*H 
+                    state_fut = X[i] - Umin + reservoir.inflow[s,k]*H 
                     if not(debut):
                         penalty = pen(state_fut)
                     if (Gs(Umin) + V_fut(state_fut)+penalty) > Vu:
                         Vu = Gs(Umin) + V_fut(state_fut)+penalty
 
-                Umax = X[i]+ apport[s,k]*H-Xmax[s]
+                Umax = X[i]+ reservoir.inflow[s,k]*H-reservoir.Xmax[s]
                 if np.min(U) <= Umax <= np.max(U):
-                    state_fut = X[i] - Umax + apport[s,k]*H 
+                    state_fut = X[i] - Umax + reservoir.inflow[s,k]*H 
                     if not(debut):
                         penalty = pen(state_fut)
                     if (Gs(Umax) + V_fut(state_fut)+penalty) > Vu:
                         Vu = Gs(Umax) + V_fut(state_fut)+penalty
             
-                V[i, s] = Vu/NTrain + V[i,s]
+                V[i, s] = Vu/study.nb_mc + V[i,s]
     return V
 
-def get_penalties(for_beginning_of_week,s):
+def get_penalties(study, reservoir, for_beginning_of_week,s):
     if for_beginning_of_week:
         if s>=1:
-            pen = interp1d([XminB,Xmin[s-1],Xmax[s-1],XmaxB],[-pen_low*(Xmin[s-1]-XminB),0,0,-pen_high*(XmaxB-Xmax[s-1])])
+            pen = interp1d([0,reservoir.Xmin[s-1],reservoir.Xmax[s-1],reservoir.capacity],[-study.pen_low*(reservoir.Xmin[s-1]),0,0,-study.pen_high*(reservoir.capacity-reservoir.Xmax[s-1])])
         else:
-            pen = interp1d([XminB,Xmin[S-1],Xmax[S-1],XmaxB],[-pen_low*(Xmin[S-1]-XminB),0,0,-pen_high*(XmaxB-Xmax[S-1])])
+            pen = interp1d([0,reservoir.Xmin[S-1],reservoir.Xmax[S-1],reservoir.capacity],[-study.pen_low*(reservoir.Xmin[S-1]),0,0,-study.pen_high*(reservoir.capacity-reservoir.Xmax[S-1])])
     else:
-        pen = interp1d([XminB,Xmin[s],Xmax[s],XmaxB],[-pen_low*(Xmin[s]-XminB),0,0,-pen_high*(XmaxB-Xmax[s])])
+        pen = interp1d([0,reservoir.Xmin[s],reservoir.Xmax[s],reservoir.capacity],[-study.pen_low*(reservoir.Xmin[s]),0,0,-study.pen_high*(reservoir.capacity-reservoir.Xmax[s])])
     return(pen)
+
+def solve_and_plot(m, study):
+    m.solve()
+    vars = m.getVariable()
+    sol = m.getSolution()
+
+    fig, ax = plt.subplots(2*len(study.list_areas), sharex='all',figsize=(7,5*len(study.list_areas)))
+    j = 0
+    for a in study.list_areas:
+        conso = [sol[i] for i in range(len(vars)) if f"load_{a.name}_" in vars[i].name]
+        ax[j].plot(conso, label="Conso")
+        prod_plus = {}
+        for th in a.list_thermal_units:
+            prod_plus[f"Prod_th_{th.name}"] = np.array([sol[i] for i in range(len(vars)) if f"t_{a.name}_{th.name}_" in vars[i].name])
+        for r in a.list_reservoirs:
+            prod_plus[f"Turb_r_{r.name}"] = np.array([max(-sol[i],0) for i in range(len(vars)) if f"r_" in vars[i].name])
+        prod_plus[f"ENS_{a.name}"] = np.array([sol[i] for i in range(len(vars)) if f"ens_{a.name}_" in vars[i].name])
+        for a2 in study.list_areas:
+            if a.name in study.links[a2.name]:
+                prod_plus[f"exchange_{a2.name}_{a.name}"] = np.array([sol[i] for i in range(len(vars)) if f"exchange_{a2.name}_{a.name}_" in vars[i].name])
+        ax[j].stackplot(np.arange(H), prod_plus.values(),
+                labels=prod_plus.keys())
+        
+        ax[j].legend()
+        ax[j].grid(True)
+        ax[j].set_title(f"{a.name}")
+        # plt.show()
+
+        # fig, ax = plt.subplots()
+        ax[j+1].plot(sum([prod_plus[a] for a in prod_plus.keys()])-conso, label="Conso-prod")
+        prod_minus = {}
+        for r in a.list_reservoirs:
+            prod_minus[f"Pump_r_{r.name}"] = np.array([max(sol[i],0) for i in range(len(vars)) if f"r_" in vars[i].name])
+        prod_minus[f"spill_{a.name}"] = np.array([-sol[i] for i in range(len(vars)) if f"spill_{a.name}_" in vars[i].name])
+        for a2 in study.list_areas:
+            if a2.name in study.links[a.name]:
+                prod_minus[f"exchange_{a.name}_{a2.name}"] = np.array([sol[i] for i in range(len(vars)) if f"exchange_{a.name}_{a2.name}_" in vars[i].name])
+        # assert(np.all(prod_plus-conso-prod_minus)==0) 
+        ax[j+1].stackplot(np.arange(H), prod_minus.values(),
+                labels=prod_minus.keys())
+        ax[j+1].legend()
+        ax[j+1].grid(True)
+        ax[j+1].set_title(f"{a.name}")
+        j += 2
+
+    plt.show()
+
+    for a in study.list_areas:
+        for r in a.list_reservoirs:
+            fig, ax = plt.subplots()
+            u = np.array([sol[i] for i in range(len(vars)) if f"r_" in vars[i].name])
+            ax.plot(np.cumsum(u)+r.initial_level, label=f"r_{r.name}")
+            ax.plot(r.Xmin[0].repeat(H))
+            ax.plot(r.Xmax[0].repeat(H))
+            ax.legend()
+            ax.set_title(f"{a.name}")
+            plt.show()
--- param_watervalues.py
+++ param_watervalues.py
@@ -3,40 +3,90 @@ import numpy as np
 H = 168
 S = 52
 
-#Thermal units and energy not served parameters (the energy not served is modeled as an expensive thermal unit with infinite capacity )
-cTh = [0.1, 0.2, 0.3, 3] # euro/MWh
-PminTh = [0.0, 0.0, 0.0, 0.0]
-PmaxTh = [42500.0, 12500.0, 7500.0, float('inf')] # MW
+# #Thermal units and energy not served parameters (the energy not served is modeled as an expensive thermal unit with infinite capacity )
+# cTh = [0.1, 0.2, 0.3, 3] # euro/MWh
+# PminTh = [0.0, 0.0, 0.0, 0.0]
+# PmaxTh = [42500.0, 12500.0, 7500.0, float('inf')] # MW
 
-# Battery
-x0 = 0.0
-PminB = -5.e4 #MW turbinage max
-PmaxB = 5.e4 # pompage max
-XminB = 0.0
-rho = 200 
-XmaxB = rho*PmaxB         #The maximum capacity changes for the different values of rho considered 
-xNsteps = 51
+# # Battery
+# x0 = 0.0
+# PminB = -5.e4 #MW turbinage max
+# PmaxB = 5.e4 # pompage max
+# XminB = 0.0
+# rho = 200 
+# XmaxB = rho*PmaxB         #The maximum capacity changes for the different values of rho considered 
+# xNsteps = 51
 
-courbes_guides = np.loadtxt("D:/Users/gerbauxjul/Documents/6-Etudes Antares/OneNodeBase_ref/input/hydro/common/capacity/reservoir_area.txt")[6:365:7,[0,2]]*XmaxB # on récupère les valeurs du dimanche
-Xmin = courbes_guides[:,0]
-Xmax = courbes_guides[:,1]
-Xmin = np.concatenate((Xmin,Xmin[[0]]))
-Xmax = np.concatenate((Xmax,Xmax[[0]]))
+# courbes_guides = np.loadtxt("D:/Users/gerbauxjul/Documents/6-Etudes Antares/OneNodeBase_ref/input/hydro/common/capacity/reservoir_area.txt")[6:365:7,[0,2]]*XmaxB # on récupère les valeurs du dimanche
+# Xmin = courbes_guides[:,0]
+# Xmax = courbes_guides[:,1]
+# Xmin = np.concatenate((Xmin,Xmin[[0]]))
+# Xmax = np.concatenate((Xmax,Xmax[[0]]))
 
-NTrain = 10
+# NTrain = 10
 
-Demand = np.loadtxt("D:/Users/gerbauxjul/Documents/6-Etudes Antares/OneNodeBase_ref/input/load/series/load_area.txt")[:8736, :NTrain]
+# Demand = np.loadtxt("D:/Users/gerbauxjul/Documents/6-Etudes Antares/OneNodeBase_ref/input/load/series/load_area.txt")[:8736, :NTrain]
 
-LAW_s = np.reshape(Demand,(S,H,NTrain)) 
-proba_s = np.ones((S,NTrain))/NTrain
+# LAW_s = np.reshape(Demand,(S,H,NTrain)) 
+# proba_s = np.ones((S,NTrain))/NTrain
 
-apport =np.loadtxt("D:/Users/gerbauxjul/Documents/6-Etudes Antares/OneNodeBase_ref/input/hydro/series/area/mod.txt")[6:365:7,:NTrain]*7/H 
+# apport =np.loadtxt("D:/Users/gerbauxjul/Documents/6-Etudes Antares/OneNodeBase_ref/input/hydro/series/area/mod.txt")[6:365:7,:NTrain]*7/H 
 
-pen_low = 3.1
-pen_high = 3.1
+# pen_low = 3.1
+# pen_high = 3.1
 
 
-X = np.linspace(XminB, XmaxB, num = xNsteps)
+class ThermalUnit:
 
-Ncontrols = 51
-U = np.linspace(-PmaxB * H, -PminB * H, Ncontrols)
+    def __init__(self, marginal_cost, P_min, P_max, name):
+        self.marginal_cost = marginal_cost
+        self.P_min = P_min
+        self.P_max = P_max
+        assert("_" not in name)
+        self.name = name
+
+    def __str__(self) -> str:
+        return(self.name)
+
+class Reservoir:
+
+    def __init__(self, initial_level, P_turb, P_pump, rho, dir_study, name_area, name):
+        self.initial_level = initial_level
+        self.P_turb = P_turb
+        self.P_pump = P_pump
+        self.capacity = -rho*P_turb
+
+        courbes_guides = np.loadtxt(dir_study+"/input/hydro/common/capacity/reservoir_"+name_area+".txt")[6:365:7,[0,2]]*self.capacity # on récupère les valeurs du dimanche
+        Xmin = courbes_guides[:,0]
+        Xmax = courbes_guides[:,1]
+        self.Xmin = np.concatenate((Xmin,Xmin[[0]]))
+        self.Xmax = np.concatenate((Xmax,Xmax[[0]]))
+
+        self.inflow = np.loadtxt(dir_study+"/input/hydro/series/"+name_area+"/mod.txt")[6:365:7]*7/H 
+        assert("_" not in name)
+        self.name = name
+
+class Area:
+
+    def __init__(self, thermal_units, reservoirs, dir_study, name_area, name):
+        self.list_thermal_units = thermal_units
+        self.list_reservoirs = reservoirs
+
+        load = np.loadtxt(dir_study+"/input/load/series/load_"+name_area+".txt")[:8736]
+        mc = load.shape[1]
+        self.load = np.reshape(load,(S,H,mc))
+        assert("_" not in name)
+        self.name = name
+
+    def __str__(self):
+        return(self.name)
+
+class Study:
+
+    def __init__(self, areas, pen_low, pen_high, cost_ens, nb_mc, links):
+        self.list_areas = areas
+        self.pen_low = pen_low
+        self.pen_high = pen_high
+        self.cost_ens = cost_ens
+        self.nb_mc = nb_mc
+        self.links = links
